2362dcd69be2a383766eb971095baba90c4ff367
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-02-28 18:20:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var data = [
+  {
+    AboutDevTypeText: "<span>- PH3NX -</span><br/><br/><h2>Python programmer and CTF lover !</h2>"
+  }
+];
+
+var allElements = document.getElementsByClassName("typeing");
+for (var j = 0; j < allElements.length; j++) {
+  var currentElementId = allElements[j].id;
+  var currentElementIdContent = data[0][currentElementId];
+  var element = document.getElementById(currentElementId);
+  var devTypeText = currentElementIdContent;
+
+  // type code
+  var i = 0, isTag, text;
+  (function type() {
+    text = devTypeText.slice(0, ++i);
+    if (text === devTypeText) return;
+    element.innerHTML = text + `<span class='blinker'>&#32;</span>`;
+    var char = text.slice(-1);
+    if (char === "<") isTag = true;
+    if (char === ">") isTag = false;
+    if (isTag) return type();
+    setTimeout(type, 60);
+  })();
+}
